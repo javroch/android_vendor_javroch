b63b0a33cfcb3b58f8ed034e013f9c200162f935
Josh Avroch <josh.avroch@gmail.com>
2012-04-12 15:33:24 -0400

    drm/google movies fix?

--- products/javroch_drm.inc.mk
+++ products/javroch_drm.inc.mk
@@ -1,3 +1,10 @@
-# Enable drm (only does anything if you've flashed gapps)
+# Enable drm / copy files not present in gapps
+PRODUCT_COPY_FILES +=  \
+    vendor/javroch/props/phone/lib/libWVphoneAPI.so:system/lib/libWVphoneAPI.so \
+    vendor/javroch/props/phone/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
+    vendor/javroch/props/phone/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/javroch/props/phone/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
+    vendor/javroch/props/phone/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so
+
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
