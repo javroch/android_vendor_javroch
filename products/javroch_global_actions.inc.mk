feb10c24fdaa3334d21858cad41646ec3f191080
Josh Avroch <josh.avroch@gmail.com>
2012-04-14 16:10:35 -0400

    adding the silent mode/airport mode toggles

--- products/javroch_global_actions.inc.mk
+++ products/javroch_global_actions.inc.mk
@@ -1,5 +1,7 @@
-# Enable extra global actions (reboot/screenshot)
+# Enable global actions toggles (reboot/screenshot/airplane mode/silent mode)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.clean.reboot=1 \
     ro.clean.screenshot=1 \
-    ro.clean.airplane_mode=1
+    ro.clean.airplane_mode=1 \
+    ro.clean.silent_mode=1
+
