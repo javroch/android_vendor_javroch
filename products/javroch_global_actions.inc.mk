ddd4242a8dbe2598c7a12b05a4da9bd1d19fea6f
Josh Avroch <josh.avroch@gmail.com>
2012-04-14 15:00:21 -0400

    adding the ability to remove the airplane mode toggle from the power menu

--- products/javroch_global_actions.inc.mk
+++ products/javroch_global_actions.inc.mk
@@ -1,4 +1,5 @@
 # Enable extra global actions (reboot/screenshot)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.clean.reboot=1 \
-    ro.clean.screenshot=1
+    ro.clean.screenshot=1 \
+    ro.clean.airplane_mode=1
